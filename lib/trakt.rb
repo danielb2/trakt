4fa0c800fd4555b1d86c6cb101ec9c8a81681446
Daniel Bretoi <daniel@bretoi.com>
2012-09-28 12:42:17 -0700

    add ability to search movies

--- lib/trakt.rb
+++ lib/trakt.rb
@@ -1,11 +1,23 @@
 require "trakt/version"
+require 'awesome_print'
+require 'open-uri'
+require "json"
 
 module Trakt
-  def set(what, value)
+  def self.set(what, value)
+  @settings = {}
     @settings[what] = value
   end
-  module Search
-    class Movie
+  def self.settings
+    @settings
+  end
+  class Movie
+    def self.get_url
+      "http://api.trakt.tv/search/movies.json/" + Trakt.settings[:apikey]
+    end
+    def self.find(search)
+      result = open(get_url + "/uncle+buck")
+      JSON.parse result.read
     end
   end
 end
