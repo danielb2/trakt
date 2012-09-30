e281b4b473fb8b0b98996683e13e57ec9ad62bcd
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 12:12:38 -0700

    adding ability add items to list etc

--- lib/trakt.rb
+++ lib/trakt.rb
@@ -2,22 +2,79 @@ require "trakt/version"
 require 'awesome_print'
 require 'open-uri'
 require "json"
+require "excon"
+require "digest"
+require "uri"
 
 module Trakt
-  def self.set(what, value)
   @settings = {}
+  def self.set(what, value)
     @settings[what] = value
   end
   def self.settings
     @settings
   end
-  class Movie
-    def self.get_url
-      "http://api.trakt.tv/search/movies.json/" + Trakt.settings[:apikey]
+  class List
+    class << self
+      def connection
+        @connection ||= Excon.new("http://api.trakt.tv");
+      end
+      # TODO options should be the various options at some point
+      def create(name, options={})
+        body = {
+          'username' => Trakt.settings[:username],
+          'password' => Trakt.settings[:password],
+          'privacy' => 'private',
+          'description' => "something here",
+          'name' => name,
+        }
+        result = connection.post(:path => "/lists/items/add/" + Trakt.settings[:apikey], :body => body.to_json)
+        if result.status == 'failure'
+          return nil
+        end
+        parsed_result = JSON.parse result.body
+        return self.new(parsed_result['slug'])
+      end
     end
-    def self.find(search)
-      result = open(get_url + "/uncle+buck")
-      JSON.parse result.read
+    attr_accessor :slug
+    def initialize(slug)
+      @slug = slug
+    end
+    def add_item(data)
+      body = {
+          'slug' => self.slug,
+          'username' => Trakt.settings[:username],
+          'password' => Trakt.settings[:password],
+          'items'    => [data],
+      }
+      result = self.class.connection.post(:path => "/lists/items/add/" + Trakt.settings[:apikey], :body => body.to_json)
+    end
+  end
+  class Movie
+    class << self
+      def connection
+        @connection ||= Excon.new("http://api.trakt.tv");
+      end
+      def search_path
+      "/search/movies.json/" + Trakt.settings[:apikey] + '/'
+      end
+      def clean_query(query)
+        query.gsub(/[()]/,'').
+        gsub(' ','+').
+        gsub('&','and').
+        gsub('!','').
+        chomp
+      end
+      def find(query)
+        puts "Searching for: #{query}"
+        begin
+          result = connection.get(:path => search_path + clean_query(query))
+          return JSON.parse result.body
+        rescue
+          puts "Something went amiss with your query: `#{query}` (#{clean_query(query)})"
+          exit;
+        end
+      end
     end
   end
 end
