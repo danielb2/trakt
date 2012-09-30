30117ec04ead72ab98cd5095767efa6d9363bc34
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 15:28:36 -0700

    make sure settings are required

--- lib/trakt/search.rb
+++ lib/trakt/search.rb
@@ -3,18 +3,23 @@ module Trakt
     extend Connection
     class << self
       def movies(query)
+        require_settings %w|apikey|
         get('/search/movies.json/',clean_query(query))
       end
       def shows(query)
+        require_settings %w|apikey|
         get('/search/shows.json/',clean_query(query))
       end
       def episode(query)
+        require_settings %w|apikey|
         get('/search/episodes.json/',clean_query(query))
       end
       def people(query)
+        require_settings %w|apikey|
         get('/search/people.json/',clean_query(query))
       end
       def users(query)
+        require_settings %w|apikey|
         get('/search/users.json/',clean_query(query))
       end
     end
