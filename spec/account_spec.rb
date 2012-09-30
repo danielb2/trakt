c8f24960cf8b1869b9b2fda9970e6bcb6c732f7c
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 14:17:10 -0700

    separate tests as appropriate

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,12 +7,10 @@ describe Trakt do
     Trakt.set :username, details['username']
     Trakt.set :password, details['password']
   end
-  describe Trakt::Movie do
-    it "should find uncle buck" do
-      result = Trakt::Movie.find("uncle buck").first
-      result['title'].should == 'Uncle Buck'
-      result['year'].should == 1989
-      result['imdb_id'].should == 'tt0098554'
+  describe Trakt::Account do
+    it "should get account info" do
+      result = Trakt::Account.settings
+      result['message'].should == "All settings for #{Trakt.settings[:username]}"
     end
   end
 end
