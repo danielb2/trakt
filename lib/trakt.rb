f121f4e30032ac2bb47114a5a8aaa1f19b77f190
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 13:34:47 -0700

    separate out list

--- lib/trakt.rb
+++ lib/trakt.rb
@@ -7,6 +7,7 @@ require "digest"
 require "uri"
 require "trakt/connection"
 require "trakt/account"
+require "trakt/list"
 
 module Trakt
   class Error < RuntimeError
@@ -19,42 +20,6 @@ module Trakt
   end
   def self.settings
     @settings
-  end
-  class List
-    class << self
-      def connection
-        @connection ||= Excon.new("http://api.trakt.tv");
-      end
-      # TODO options should be the various options at some point
-      def create(name, options={})
-        body = {
-          'username' => Trakt.settings[:username],
-          'password' => Trakt.settings[:password],
-          'privacy' => 'private',
-          'description' => "something here",
-          'name' => name,
-        }
-        result = connection.post(:path => "/lists/items/add/" + Trakt.settings[:apikey], :body => body.to_json)
-        if result.status == 'failure'
-          return nil
-        end
-        parsed_result = JSON.parse result.body
-        return self.new(parsed_result['slug'])
-      end
-    end
-    attr_accessor :slug
-    def initialize(slug)
-      @slug = slug
-    end
-    def add_item(data)
-      body = {
-          'slug' => self.slug,
-          'username' => Trakt.settings[:username],
-          'password' => Trakt.settings[:password],
-          'items'    => [data],
-      }
-      result = self.class.connection.post(:path => "/lists/items/add/" + Trakt.settings[:apikey], :body => body.to_json)
-    end
   end
   class Movie
     extend Connection
