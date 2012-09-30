29298763a177963fc3d11afce80cd81bbb47c540
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 14:14:11 -0700

    put login details in a separate file so I can checkin tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require "rspec"
 require "trakt"
+require "awesome_print"
+require "yaml"
+
+def get_account_details
+  account = IO.read File.dirname(__FILE__) + '/account_details.yml'
+  YAML.load(account)
+end
