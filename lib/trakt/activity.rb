0c38c3a428f50001593e45e30899f9fe4d6ee49f
Daniel Bretoi <daniel@bretoi.com>
2012-09-30 23:21:40 -0700

    more documentation

--- lib/trakt/activity.rb
+++ lib/trakt/activity.rb
@@ -1,4 +1,10 @@
 module Trakt
+  # Refer to the api doc on what parameters these functions take. http://trakt.tv/api-docs/activity-community
+  # For eaxmple, the current description for community reads:
+  #
+  #     http://api.trakt.tv/activity/community.format/apikey/types/actions/start_ts/end_ts
+  #
+  # So you just do: <tt>Trakt::Activity.community(<types>,<actions>,<start_ts>,<end_ts>)</tt>
   class Activity
     extend Connection
     class << self
