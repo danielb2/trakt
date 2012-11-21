837a5fef9039b3ad0f4139286ec3a3ceaa95f3f7
Daniel Bretoi <daniel@bretoi.com>
2012-11-20 20:33:30 -0800

    move fixtures around

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ end
 
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.allow_http_connections_when_no_cassette = true
   c.hook_into :excon
 end
