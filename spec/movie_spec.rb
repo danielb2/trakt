0078c7399ef9b16e26c05c907fb15ad0d238b39c
Daniel Bretoi <daniel@bretoi.com>
2012-11-20 20:24:48 -0800

    more speeding up

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -10,13 +10,15 @@ describe Trakt do
       trakt
     end
     it "should mark twelve monkeys" do
-      result = trakt.movie.seen  movies: [{
-            imdb_id: "tt0114746",
-            title: "Twelve Monkeys",
-            year: 1995,
-            plays: 1,
-            last_played: 1255960578
-      }]
+      result = record(self) do
+        trakt.movie.seen  movies: [{
+          imdb_id: "tt0114746",
+          title: "Twelve Monkeys",
+          year: 1995,
+          plays: 1,
+          last_played: 1255960578
+        }]
+      end
       result['status'].should == 'success'
     end
   end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -10,31 +10,41 @@ describe Trakt do
       trakt
     end
     it "should find movie uncle buck" do
-      result = trakt.search.movies("uncle buck").first
+      result =  record(self) do
+        trakt.search.movies("uncle buck").first
+      end
       result['title'].should == 'Uncle Buck'
       result['year'].should == 1989
       result['imdb_id'].should == 'tt0098554'
     end
     it "should find show cowboy bebop" do
-      result = trakt.search.shows("cowboy bebop").first
+      result = record(self) do
+        trakt.search.shows("cowboy bebop").first
+      end
       result['title'].should == 'Cowboy Bebop'
       result['year'].should == 1998
       result['imdb_id'].should == 'tt0213338'
     end
     it "should find episode Lisa the Vegetarian" do
-      result = trakt.search.episode("lisa the vegetarian").first
+      result = record(self) do
+        trakt.search.episode("lisa the vegetarian").first
+      end
       result['show']['title'].should == 'The Simpsons'
       result['episode']['title'].should == 'Lisa the Vegetarian'
       result['show']['year'].should == 1989
       result['show']['imdb_id'].should == 'tt0096697'
     end
     it "should find actor Eric Bana" do
-      result = trakt.search.people("eric bana").first
+      result = record(self) do
+        trakt.search.people("eric bana").first
+      end
       result['name'].should == 'Eric Bana'
       result['birthplace'].should == 'Melbourne, Australia'
     end
     it "should find user infodump" do
-      result = trakt.search.users("infodump").first
+      result = record(self) do
+        trakt.search.users("infodump").first
+      end
       result['username'].should == 'infodump'
       result['joined'].should == 1320907373
     end
